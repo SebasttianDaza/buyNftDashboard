93947ba8f2ca8c64d4aca924189439d90a4f021a
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-04-06 08:19:34 -0500

    Initial repo
    it started the strcuture of the project with the tools

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "import"],
+  rules: {
+    "no-console": "off",
+    "react/react-in-jsx-scope": 0,
+    "prettier/prettier": [
+      "warn",
+      {
+        printWidth: 100,
+        trailingComma: "all",
+        tabWidth: 2,
+        semi: true,
+        singleQuote: false,
+        bracketSpacing: false,
+        arrowParens: "always",
+        endOfLine: "auto",
+      },
+    ],
+    "import/order": ["warn", {"newlines-between": "always"}],
+    "react/self-closing-comp": "warn",
+  },
+};
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hola</h1>
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
